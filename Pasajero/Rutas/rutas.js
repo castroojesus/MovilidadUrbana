453d9b7fb88da92d5d5fccfe2d787b00b46d709d
castroojesus <jesusacs1111@gmail.com>
2026-05-02 12:05:05 -0700

    planificador y favoritos

--- Pasajero/Rutas/rutas.js
+++ Pasajero/Rutas/rutas.js
@@ -1,5 +1,12 @@
 
 
+const urlParams = new URLSearchParams(window.location.search);
+const rutaId = urlParams.get('id');
+
+if (rutaId) {
+    console.log("Cargando detalles para la ruta: " + rutaId);
+    
+}
     
     const tabs = document.querySelectorAll('.tab');
 
